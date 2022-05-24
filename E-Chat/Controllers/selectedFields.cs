19e739c73678155208243b18de60026f90f5ca34
EdenHamami <92511329+EdenHamami@users.noreply.github.com>
2022-05-24 17:55:27 +0300

    im living the dream

---
+++ E-Chat/Controllers/selectedFields.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace E_Chat.Controllers
+{
+    internal class selectedFields
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Server { get; set; }
+
+        public string Last { get; set; }
+        public string Lastdate { get; set; }
+    }
+}
